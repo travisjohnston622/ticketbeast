bbd2a5291eb4760ebea508345581147d0fc76575
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-08 14:09:03 -0500

    concert class test

---
+++ tests/features/ViewConcertListingTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Concert;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewConcertListingTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function user_can_view_a_concert_listing()
+    {
+        //Create a concert
+
+        $concert = Concert::create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('May 7, 2021 8:00pm'),
+            'ticket_price' => 3250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555',
+        ]);
+
+        //View the concert listing
+
+        $this->visit('/concerts/'.$concert->id);
+
+        //See the concert details
+
+        $this->see('The Red Chord');
+        $this->see('with Animosity and Lethargy');
+        $this->see('May 7, 2021');
+        $this->see('8:00pm');
+        $this->see('32.50');
+        $this->see('The Mosh Pit');
+        $this->see('123 Example Lane');
+        $this->see('Laraville, ON 17916');
+        $this->see('For tickets, call (555) 555-5555');
+    }
+}
