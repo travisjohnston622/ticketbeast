d8d8a004dd81af3d35d9e39b712d903c586eb269
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-11 16:30:38 -0500

    Update ConcertOrdersController.php

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Billing\PaymentFailedException;
 use App\Billing\PaymentGateway;
 use App\Concert;
 use Illuminate\Http\Request;
@@ -17,13 +18,19 @@ class ConcertOrdersController extends Controller
 
     public function store($concertId)
     {
-        $concert = Concert::find($concertId);
+        $concert = Concert::published()->findOrFail($concertId);
+        $this->validate(request(), [
+            'email' => ['required', 'email'],
+            'ticket_quantity' => ['required', 'integer', 'min:1'],
+            'payment_token' => ['required'],
+        ]);
 
-        $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
+        try {
+            $this->paymentGateway->charge(request('ticket_quantity') * $concert->ticket_price, request('payment_token'));
+            $order = $concert->orderTickets(request('email'), request('ticket_quantity'));
+        } catch (PaymentFailedException $e) {
+            return response()->json([], 422);
+        }
 
-        $order = $concert->orderTickets(request('email'), request('ticket_quantity'));
-
-
-        return response()->json([], 201);
     }
 }
