3a1006bd03d8eb9c018331dae1f030c3d54e8a40
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-11 10:19:14 -0500

    Update ConcertTest.php

--- tests/unit/ConcertTest.php
+++ tests/unit/ConcertTest.php
@@ -2,9 +2,13 @@
 
 use App\Concert;
 use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ConcertTest extends TestCase
 {
+    use DatabaseMigrations;
 
     /** @test */
     function can_get_formatted_date()
@@ -13,7 +17,7 @@ class ConcertTest extends TestCase
             'date' => Carbon::parse('2021-05-10')
     ]);
 
-        $this->assertEquals('May 10, 2021', $concert->formatted_date);
+        $this->assertEquals('May 10, 2021', $concert->getFormattedDate());
     }
 
     /** @test */
@@ -23,7 +27,7 @@ class ConcertTest extends TestCase
             'date' => Carbon::parse('2021-05-10 17:00:00')
         ]);
 
-        $this->assertEquals('5:00pm', $concert->formatted_start_time);
+        $this->assertEquals('5:00pm', $concert->getFormattedStartTime());
     }
 
     /** @test */
@@ -33,6 +37,20 @@ class ConcertTest extends TestCase
             'ticket_price' => 6750
         ]);
 
-        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
+        $this->assertEquals('67.50', $concert->getTicketPriceInDollars());
+    }
+
+    /** @test */
+    function concerts_with_a_published_at_date_are_published()
+    {
+        $publishedConcertA = factory(Concert::class)->create(['published_at' => Carbon::parse('-1 week')]);
+        $publishedConcertB = factory(Concert::class)->create(['published_at' => Carbon::parse('-1 week')]);
+        $unpublishedConcert = factory(Concert::class)->create(['published_at' => null]);
+
+        $publishedConcerts = Concert::published()->get();
+
+        $this->assertTrue($publishedConcerts->contains($publishedConcertA));
+        $this->assertTrue($publishedConcerts->contains($publishedConcertB));
+        $this->assertFalse($publishedConcerts->contains($unpublishedConcert));
     }
 }
