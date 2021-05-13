2c5b16acb3a9bd74faa1b73f7651e5c5d14a8a28
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-13 12:41:13 -0500

    Update Order.php

--- app/Order.php
+++ app/Order.php
@@ -8,8 +8,50 @@ class Order extends Model
 {
     protected $guarded = [];
 
+    public static function forTickets($tickets, $email, $amount)
+    {
+        $order = self::create([
+            'email' => $email,
+            'amount' => $amount,
+        ]);
+
+        foreach ($tickets as $ticket) {
+            $order->tickets()->save($ticket);
+        }
+
+        return $order;
+    }
+
+    public function concert()
+    {
+        return $this->belongsTo(Concert::class);
+    }
+
     public function tickets()
     {
         return $this->hasMany(Ticket::class);
+    }
+
+    public function ticketQuantity()
+    {
+        return $this->tickets()->count();
+    }
+
+    public function cancel()
+    {
+        foreach ($this->tickets as $ticket) {
+            $ticket->release();
+        }
+
+        $this->delete();
+    }
+
+    public function toArray()
+    {
+        return [
+            'email' => $this->email,
+            'ticket_quantity' => $this->ticketQuantity(),
+            'amount' => $this->amount,
+        ];
     }
 }
