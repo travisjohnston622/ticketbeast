5dbad4025bc1ea385dc248a787f4771b8df1e107
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-13 12:41:17 -0500

    Update Ticket.php

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function scopeAvailable($query)
+    {
+        return $query->whereNull('order_id');
+    }
+
+    public function release()
+    {
+        $this->update(['order_id' => null]);
+    }
+
+    public function concert()
+    {
+        return $this->belongsTo(Concert::class);
+    }
+
+    public function getPriceAttribute()
+    {
+        return $this->concert->ticket_price;
+    }
 }
