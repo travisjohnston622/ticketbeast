97355abca2611bd36de0dd262b58d6542c40b777
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-17 12:12:58 -0500

    Update FakePaymentGatewayTest.php

--- tests/unit/Billing/FakePaymentGatewayTest.php
+++ tests/unit/Billing/FakePaymentGatewayTest.php
@@ -25,9 +25,27 @@ class FakePaymentGatewayTest extends TestCase
             $paymentGateway = new FakePaymentGateway();
             $paymentGateway->charge(2500, 'invalid-payment-token');
         } catch (PaymentFailedException $e) {
+            $this->assertTrue(true);
             return;
         }
 
         $this->fail();
+    }
+
+    /** @test */
+    function running_a_hook_before_the_first_charge()
+    {
+            $paymentGateway = new FakePaymentGateway();
+            $timesCallbackRan = 0;
+
+            $paymentGateway->beforeFirstCharge(function ($paymentGateway) use (&$timesCallbackRan) {
+                $timesCallbackRan++;
+                $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+                $this->assertEquals(2500, $paymentGateway->totalCharges());
+            });
+
+            $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+            $this->assertEquals(1, $timesCallbackRan);
+            $this->assertEquals(5000, $paymentGateway->totalCharges());
     }
 }
