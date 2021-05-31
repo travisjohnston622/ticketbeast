406515fa2c8cf25cf46f257715feddb0a2668585
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-31 15:02:52 -0500

    Update ModelFactory.php

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -68,3 +68,14 @@ $factory->state(App\Ticket::class, 'reserved', function ($faker) {
         'reserved_at' => Carbon::now(),
     ];
 });
+
+$factory->define(App\Order::class, function (Faker\Generator $faker) {
+
+    return [
+        'amount' => 5250,
+        'email' => 'somebody@example.com',
+        'confirmation_number' => 'ORDERCONFIRMATION1234',
+        'card_last_four' => '1234',
+    ];
+});
+
