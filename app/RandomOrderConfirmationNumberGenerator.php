a679e34a315f3782320423df9e4e61e5fd322392
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-31 15:06:20 -0500

    created the ability to generate random confirmation numbers for orders

---
+++ app/RandomOrderConfirmationNumberGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class RandomOrderConfirmationNumberGenerator implements OrderConfirmationNumberGenerator
+{
+
+    public function generate()
+    {
+        $pool = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+
+        return substr(str_shuffle(str_repeat($pool, 24)), 0, 24);
+    }
+}
