1bcda14a48d8286ae8a55465d1aa2a64e38eb8b6
Travis Johnston <travisjohnston4588@icloud.com>
2021-05-31 15:11:30 -0500

    Create OrderTest.php

---
+++ tests/unit/OrderTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use App\Billing\Charge;
+use App\Concert;
+use App\Facades\OrderConfirmationNumber;
+use App\Order;
+use App\Ticket;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function creating_an_order_from_tickets_email_and_charge()
+    {
+        $charge = new Charge(['amount' => 3600, 'card_last_four' => '1234']);
+        $tickets = collect([
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
+        ]);
+
+        $order = Order::forTickets($tickets, 'travis@example.com', $charge);
+
+        $this->assertEquals('travis@example.com', $order->email);
+        $this->assertEquals(3600, $order->amount);
+        $this->assertEquals('1234', $order->card_last_four);
+        $tickets->each->shouldHaveReceived('claimFor', [$order]);
+    }
+
+    /** @test */
+    function retrieving_an_order_by_confirmation_number()
+    {
+        $order = factory(Order::class)->create([
+            'confirmation_number' => 'ORDERCONFIRMATION1234'
+        ]);
+
+        $foundOrder = Order::findByConfirmationNumber('ORDERCONFIRMATION1234');
+
+        $this->assertEquals($order->id, $foundOrder->id);
+    }
+
+    /** @test */
+    function retrieving_a_nonexistent_order_by_confirmation_number_throws_an_exception()
+    {
+        try {
+            Order::findByConfirmationNumber('NONEXISTENTCONFIRMATIONNUMBER');
+
+        } catch (ModelNotFoundException $e) {
+            $this->assertTrue(true);
+            return;
+        }
+
+        $this->fail('No matching order was found for the specified confirmation number, but an exception was not thrown');
+    }
+
+    /** @test */
+    function converting_to_an_array()
+    {
+        $order = factory(Order::class)->create([
+            'confirmation_number' =>  'ORDERCONFIRMATION1234',
+            'email' => 'travis@example.com',
+            'amount' => 6000
+        ]);
+        $order->tickets()->saveMany([
+            factory(Ticket::class)->create(['code' => 'TICKETCODE1']),
+            factory(Ticket::class)->create(['code' => 'TICKETCODE2']),
+            factory(Ticket::class)->create(['code' => 'TICKETCODE3']),
+        ]);
+
+        $result = $order->toArray();
+
+        $this->assertEquals([
+            'confirmation_number' => 'ORDERCONFIRMATION1234',
+            'email' => 'travis@example.com',
+            'amount' => 6000,
+            'tickets' => [
+                ['code' => 'TICKETCODE1'],
+                ['code' => 'TICKETCODE2'],
+                ['code' => 'TICKETCODE3'],
+            ]
+        ], $result);
+    }
+}
