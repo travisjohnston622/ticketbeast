a93e67832d889fe85815b6573bb44f1f945787a6
Travis Johnston <travisjohnston4588@icloud.com>
2021-06-04 11:57:04 -0500

    created routes for promoters to list concerts

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@
 |
 */
 
-Route::get('/concerts/{id}', 'ConcertsController@show');
-
+Route::get('/concerts/{id}', 'ConcertsController@show')->name('concerts.show');
 Route::post('/concerts/{id}/orders', 'ConcertOrdersController@store');
-
 Route::get('/orders/{confirmationNumber}', 'OrdersController@show');
+
+Route::post('/login', 'Auth\LoginController@showLoginForm')->name('auth.show-login');
+Route::post('/login', 'Auth\LoginController@login')->name('auth.login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('auth.logout');
+
+Route::group(['middleware' => 'auth', 'prefix' => 'backstage', 'namespace' => 'Backstage'], function () {
+    Route::get('/concerts', 'ConcertsController@index');
+    Route::get('/concerts/new', 'ConcertsController@create');
+    Route::post('/concerts', 'ConcertsController@store');
+});
