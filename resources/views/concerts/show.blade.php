22fbca293767b73c36f57baaa4d1f1c9f2d5c023
Travis Johnston <travisjohnston4588@icloud.com>
2021-06-07 11:41:37 -0500

    Update show.blade.php

--- resources/views/concerts/show.blade.php
+++ resources/views/concerts/show.blade.php
@@ -1,8 +1,8 @@
 <h1>{{ $concert->title }}</h1>
 <h2>{{ $concert->subtitle }}</h2>
-<p>{{ $concert->formatted_date }}</p>
-<p>Doors open at {{ $concert->formatted_start_time }}</p>
-<p>{{ $concert->ticket_price_in_dollars }}</p>
+<p>{{ $concert->getFormattedDate() }}</p>
+<p>Doors open at {{ $concert->getFormattedStartTime() }}</p>
+<p>{{ $concert->getTicketPriceInDollars() }}</p>
 <p>{{ $concert->venue }}</p>
 <p>{{ $concert->venue_address }}</p>
 <p>{{ $concert->city }}, {{ $concert->state }} {{ $concert->zip }}</p>
