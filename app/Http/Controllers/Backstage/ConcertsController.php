0509cb2c56f119cd424d39d4816b52915895064b
Travis Johnston <travisjohnston4588@icloud.com>
2021-06-07 11:43:33 -0500

    Create ConcertsController.php

---
+++ app/Http/Controllers/Backstage/ConcertsController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Backstage;
+
+use App\Concert;
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ConcertsController extends Controller
+{
+    public function index()
+    {
+        return view('backstage.concerts.index', ['concerts' => Auth::user()->concerts]);
+    }
+
+    public function create()
+    {
+        return view('backstage.concerts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate(request(), [
+            'title' => ['required'],
+            'date' => ['required', 'date'],
+            'time' => ['required', 'date_format:g:ia'],
+            'ticket_price' => ['required', 'numeric', 'min:5'],
+            'ticket_quantity' => ['required', 'numeric', 'min:1'],
+            'venue' => ['required'],
+            'venue_address' => ['required'],
+            'city' => ['required'],
+            'state' => ['required'],
+            'zip' => ['required'],
+        ]);
+
+        $concert = Auth::user()->concerts()->create([
+            'title' => request('title'),
+            'subtitle' => request('subtitle'),
+            'date' => Carbon::parse(vsprintf('%s %s', [
+                request('date'),
+                request('time')
+            ])),
+            'ticket_price' => request('ticket_price') * 100,
+            'venue' => request('venue'),
+            'venue_address' => request('venue_address'),
+            'city' => request('city'),
+            'state' => request('state'),
+            'zip' => request('zip'),
+            'additional_information' => request('additional_information'),
+        ])->addTickets(request('ticket_quantity'));
+
+        $concert->publish();
+
+        return redirect()->route('concerts.show', $concert);
+    }
+}
