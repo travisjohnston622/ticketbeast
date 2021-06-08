6b4ffc0243b3a7762d8c568736750d986d6a0623
Travis Johnston <travisjohnston4588@icloud.com>
2021-06-08 13:33:49 -0500

    Create PublishedConcertsController.php

---
+++ app/Http/Controllers/Backstage/PublishedConcertsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Backstage;
+
+use App\Concert;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PublishedConcertsController extends Controller
+{
+    public function store()
+    {
+        $concert = Auth::user()->concerts()->findOrFail(request('concert_id'));
+
+        if ($concert->isPublished()) {
+            abort(422);
+        }
+
+        $concert->publish();
+        return redirect()->route('backstage.concerts.index');
+    }
+}
