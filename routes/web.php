c21dec5f832dfc04037159c1d5e0fe4c566afc4e
Travis Johnston <travisjohnston4588@icloud.com>
2021-06-08 13:33:31 -0500

    updated routes to include published-concerts

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,11 @@ Route::post('/login', 'Auth\LoginController@login')->name('auth.login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('auth.logout');
 
 Route::group(['middleware' => 'auth', 'prefix' => 'backstage', 'namespace' => 'Backstage'], function () {
-    Route::get('/concerts', 'ConcertsController@index');
-    Route::get('/concerts/new', 'ConcertsController@create');
+    Route::get('/concerts', 'ConcertsController@index')->name('backstage.concerts.index');
+    Route::get('/concerts/new', 'ConcertsController@create')->name('backstage.concerts.create');
     Route::post('/concerts', 'ConcertsController@store');
+    Route::get('/concerts/{id}/edit', 'ConcertsController@edit')->name('backstage.concerts.edit');
+    Route::patch('/concerts/{id}', 'ConcertsController@update')->name('backstage.concerts.update');
+    Route::post('/published-concerts', 'PublishedConcertsController@store');
 });
+
