09a47760886665394aaa3cc15b62aea82e483993
Travis Johnston <travisjohnston4588@icloud.com>
2021-06-08 13:34:16 -0500

    created ConcertFactory for published and unpublished concerts

---
+++ database/ConcertFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Concert;
+
+class ConcertFactory
+{
+    public static function createPublished($overrides = [])
+    {
+        $concert = factory(Concert::class)->create($overrides);
+        $concert->publish();
+        return $concert;
+    }
+
+    public static function createUnpublished($overrides = [])
+    {
+        return factory(Concert::class)->states('unpublished')->create($overrides);;
+    }
+}
